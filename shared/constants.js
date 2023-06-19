6cef73c48cc7495d9d07b63dbf0108d309faa0ea
pavlos noulis <pavlos.noulis@gmail.com>
2023-06-19 22:39:27 +0300

    upstream updates

--- shared/constants.js
+++ shared/constants.js
@@ -1,3 +1,13 @@
 const MAX_TEAM_SIZE = 6;
 
-export { MAX_TEAM_SIZE };
+const WRISTBAND_COLORS = [
+  "black",
+  "red",
+  "purple",
+  "green",
+  "yellow",
+  "blue",
+  "orange",
+];
+
+export { MAX_TEAM_SIZE, WRISTBAND_COLORS };
