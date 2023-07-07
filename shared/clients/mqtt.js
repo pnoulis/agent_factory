4b709642238c418b032e6916379c9cf512ebc20b
pnoul <pavlos.noulis@gmail.com>
2023-07-07 16:40:54 +0300

    mysql truncates backenddb

--- shared/clients/mqtt.js
+++ shared/clients/mqtt.js
@@ -7,6 +7,19 @@ if (ENVIRONMENT.RUNTIME === "node") {
   MQTT_CLIENT_LIB = await import("precompiled-mqtt");
 }
 
+/**
+ * Connect to a mqtt server
+ *
+ * @param {string} url - mqtt server host
+ * @returns {mqttClient} - callback based
+ *
+ * @example
+ * import { mqttClient } from './mqtt.js';
+ *
+ * const mqttClientBackend = mqttClient(MQTT_LOGIN_BACKEND_URL);
+ *
+ * mqttClientBackend.once('connect', () => console.log('connected'));
+ **/
 function mqttClient(url) {
   return new MQTT_CLIENT_LIB.connect(url);
 }
--- shared/clients/mysql.js
+++ shared/clients/mysql.js
@@ -1,7 +1,33 @@
-import * as MYSQLDB_CLIENT_LIB from "mysql2";
+import * as MYSQLDB_CLIENT_LIB from "mysql2/promise";
 
-function mysqlClient(url) {
-  return MYSQLDB_CLIENT_LIB.createConnection(url);
+/**
+ * Connect to a mysql server
+ *
+ * @param {string} url - mysql server host
+ * @returns {Promise<mysqlClient>} - promise based
+ *
+ * @example
+ * import { mysqlClient } from './mysql.js'
+ *
+ * const mysqlClientBackend = await mysqlClient(MYSQLDB_LOGIN_BACKEND_URL);
+ *
+ * await connection.query('SELECT *...')
+ *
+ **/
+
+function mysqlClient(url, options = {}) {
+  let urlparams = "";
+  let connstr = url;
+
+  for (const [key, value] of Object.entries(options)) {
+    urlparams += `${key}=${value}&`;
+  }
+
+  if (urlparams) {
+    connstr += `?${urlparams.slice(0, -1)}`;
+  }
+
+  return MYSQLDB_CLIENT_LIB.createConnection(connstr);
 }
 
 export { mysqlClient };
