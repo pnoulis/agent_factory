743121c3b189f10cb8f16f6134b0d1558adf1c0c
pnoul <pavlos.noulis@gmail.com>
2023-12-11 16:15:24 +0200

    Statically injected env at build time

--- in.vite.config.js
+++ in.vite.config.js
@@ -3,8 +3,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
+const __STATIC_ENV__ = {
+  MODE: "IN_MODE",
+  NODE_ENV: "IN_NODE_ENV",
+  LOGLEVEL: "IN_LOGLEVEL",
+  DEVICE_ID: "IN_DEVICE_ID",
+  DEVICE_TYPE: "IN_DEVICE_TYPE",
+  DEVICE_ROOMNAME: "IN_DEVICE_ROOMNAME",
+  AFADMIN_SERVER_URL: "IN_AFADMIN_SERVER_URL_ORIGIN",
+};
+
 // https:vitejs.dev/config
 export default defineConfig({
+  define: {
+    __STATIC_ENV__,
+  },
   base: "IN_AFADMIN_CLIENT_URL_PUBLIC_BASENAME",
   plugins: [react(), svgr()],
   build: {
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
+import { ENV } from "./config";
 
+console.log(ENV);
 ReactDOM.createRoot(document.getElementById("app-react-root")).render(
   <React.StrictMode>
     <div>hello world!</div>
