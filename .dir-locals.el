e78a059aefa3f111bdffa924554862d9cc30fc81
pavlos noulis <pavlos.noulis@gmail.com>
2023-12-11 23:03:57 +0200

    Track .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((projectile-globally-ignored-directories . ("backend"))
+         (projectile-globally-uningnored-directories . ("tmp"))
+         (projectile-indexing-method . hybrid)
+         (projectile-enable-caching . t)
+         (projectile-sort-order . recentf))))
