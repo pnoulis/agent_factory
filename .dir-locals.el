67fd13cae3d5c928b8f052b7bc5b07e262b779cd
pnoul <pavlos.noulis@gmail.com>
2023-12-13 10:48:33 +0200

    Fixed projectile variable syntax

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,7 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((projectile-globally-ignored-directories . ("backend"))
-         (projectile-globally-uningnored-directories . ("tmp"))
+         (projectile-globally-unignored-directories . ("tmp"))
          (projectile-indexing-method . hybrid)
          (projectile-enable-caching . t)
          (projectile-sort-order . recentf))))
