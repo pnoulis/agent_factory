a7a5c62347b36ac58238d904d03b03af6009984d
pavlos noulis <pavlos.noulis@gmail.com>
2023-12-16 01:43:22 +0200

    Wristband.random().fill().toObject()

--- src/wristband/Wristband.js
+++ src/wristband/Wristband.js
@@ -1,20 +1,37 @@
 import { Eventful } from "../Eventful.js";
 import { WRISTBAND_COLORS } from "../constants.js";
+import { random } from "./random.js";
 
 class Wristband extends Eventful {
+  static random = random;
+
   constructor(wristband) {
     super(["change"]);
     wristband ??= {};
     this.id = wristband.id ?? wristband.wristbandNumber ?? null;
     this.colorCode = wristband.colorCode ?? wristband.wristbandColor ?? null;
-    this.color = WRISTBAND_COLORS[this.colorCode];
+    this.color =
+      this.colorCode != null ? WRISTBAND_COLORS[this.colorCode] : null;
   }
-
   pair() {
     debug(`pair: ${Wristband.name}`);
   }
   unpair() {
     debug(`unpair: ${Wristband.name}`);
+  }
+
+  random(sources, options) {
+    return Wristband.random(sources, options);
+  }
+  fill(sources = [], options) {
+    return Object.assign(this, Wristband.random([this, ...sources], options));
+  }
+  toObject() {
+    return {
+      id: this.id,
+      colorCode: this.colorCode,
+      color: this.color,
+    };
   }
 }
 
---
+++ src/wristband/random.js
@@ -0,0 +1,21 @@
+import { WRISTBAND_COLORS, MAX_WRISTBAND_ID } from "../constants.js";
+import { randomInteger, isArray } from "js_utils/misc";
+
+function random(sources, options = {}) {
+  if (!isArray(sources)) {
+    sources = [sources];
+  }
+
+  let target = {};
+  for (let i = 0; i < sources.length; i++) {
+    target = {
+      ...target,
+      ...sources[i],
+    };
+  }
+
+  target.id ??= randomInteger(1, MAX_WRISTBAND_ID);
+  target.colorCode ??= randomInteger(1, WRISTBAND_COLORS.length - 1);
+  target.color = WRISTBAND_COLORS[target.colorCode];
+  return target;
+}
