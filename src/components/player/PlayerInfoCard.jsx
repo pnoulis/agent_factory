00fd7e81301a8ce9bfc72b25473e6a04110deed7
pavlos noulis <pavlos.noulis@gmail.com>
2023-12-19 00:17:19 +0200

    PlayerInfoCard merges name and surname tuples into fullname

--- src/components/player/PlayerInfoCard.jsx
+++ src/components/player/PlayerInfoCard.jsx
@@ -1,58 +1,58 @@
 import { DataTuple } from "../tuple/DataTuple.jsx";
 import { StandardDataTuple } from "../tuple/StandardDataTuple.jsx";
+import { StandardStateTuple } from "../tuple/StandardStateTuple.jsx";
 import { Card } from "../Card.jsx";
 import { useContextPlayer } from "../../contexts/ContextPlayer.jsx";
 import styled from "styled-components";
 import { WristbandInfoCard } from "../wristband/WristbandInfoCard.jsx";
+import { mergec } from "../../misc/misc.js";
 
 function PlayerInfoCard({ className }) {
   const ctx = useContextPlayer();
 
   return (
-    <InfoCard className={className}>
-      <StandardDataTuple className="player-username">
-        <DataTuple src={ctx} name="username" />
-      </StandardDataTuple>
+    <InfoCard className={mergec(className, "player-info-card")}>
       <StandardDataTuple className="player-name">
         <DataTuple src={ctx} name="name" />
+        <span className="sep"></span>
+        <DataTuple nok src={ctx} name="surname" />
       </StandardDataTuple>
-      <StandardDataTuple className="player-surname">
-        <DataTuple src={ctx} name="surname" />
+      <StandardDataTuple className="player-username">
+        <DataTuple src={ctx} name="username" />
       </StandardDataTuple>
       <StandardDataTuple className="player-email">
         <DataTuple src={ctx} name="email" />
       </StandardDataTuple>
-      <StandardDataTuple className="player-password">
-        <DataTuple src={ctx} name="password" />
-      </StandardDataTuple>
+      <StandardStateTuple className="player-state">
+        <DataTuple src={ctx} name="state" label="status" />
+      </StandardStateTuple>
       <WristbandInfoCard />
     </InfoCard>
   );
 }
 
 const InfoCard = styled(Card)`
-  width: max-content;
-  padding: 10px;
+  max-width: 570px;
+  min-height: 120px;
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-auto-rows: 1fr;
   column-gap: 10px;
   align-items: center;
 
-  .wristband-info-card {
-    padding: 10px;
-    width: min-content;
-    margin-left: auto;
+  .player-state {
     grid-column: 2 / 3;
-    grid-row: 1 / 6;
-    align-self: center;
+    grid-row: 1 / 2;
+    justify-self: end;
   }
 
-  .wristband-color {
-    display: none;
-  }
-  .wristband-id {
-    grid-row: 1 / 3;
+  .wristband-info-card {
+    padding: 5px 15px;
+    max-width: 220px;
+    margin-left: auto;
+    grid-column: 2 / 3;
+    grid-row: 2 / 4;
+    row-gap: 0;
   }
 `;
 
