85923816ea2de9b18797c156dba4797ca33439a7
pavlos noulis <pavlos.noulis@gmail.com>
2023-12-19 00:14:24 +0200

    Card padding decreased to 15px

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Card = styled("article")`
   background-color: var(--grey-light);
   border-radius: var(--br-lg);
-  padding: 20px;
+  padding: 15px;
   height: 100%;
   width: 100%;
 `;
