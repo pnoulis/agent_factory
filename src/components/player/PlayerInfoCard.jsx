63aeb1846326cf9bb0f0c0494fd8767c7758cf1a
pnoul <pavlos.noulis@gmail.com>
2023-12-18 17:08:57 +0200

    PlayerInfoCard includes WristbandInfoCard

--- src/components/player/PlayerInfoCard.jsx
+++ src/components/player/PlayerInfoCard.jsx
@@ -3,6 +3,7 @@ import { StandardDataTuple } from "../tuple/StandardDataTuple.jsx";
 import { Card } from "../Card.jsx";
 import { useContextPlayer } from "../../contexts/ContextPlayer.jsx";
 import styled from "styled-components";
+import { WristbandInfoCard } from "../wristband/WristbandInfoCard.jsx";
 
 function PlayerInfoCard({ className }) {
   const ctx = useContextPlayer();
@@ -24,17 +25,35 @@ function PlayerInfoCard({ className }) {
       <StandardDataTuple className="player-password">
         <DataTuple src={ctx} name="password" />
       </StandardDataTuple>
+      <WristbandInfoCard />
     </InfoCard>
   );
 }
 
 const InfoCard = styled(Card)`
+  width: max-content;
+  padding: 10px;
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-auto-rows: 1fr;
-  column-gap: 30px;
-  align-items: start;
-  row-gap: 5px;
+  column-gap: 10px;
+  align-items: center;
+
+  .wristband-info-card {
+    padding: 10px;
+    width: min-content;
+    margin-left: auto;
+    grid-column: 2 / 3;
+    grid-row: 1 / 6;
+    align-self: center;
+  }
+
+  .wristband-color {
+    display: none;
+  }
+  .wristband-id {
+    grid-row: 1 / 3;
+  }
 `;
 
 export { PlayerInfoCard };
