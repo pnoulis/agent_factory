e55506c65003e5d44748d17499fbd5112f3b7718
pavlos noulis <pavlos.noulis@gmail.com>
2023-12-19 11:55:36 +0200

    Added links

---
+++ src/links.jsx
@@ -0,0 +1,15 @@
+const links = {
+  home: {
+    path: "/",
+    label: "home",
+  },
+  team: {
+    path: function (teamid) {},
+  },
+  merge: {
+    path: "merge",
+    label: "merge",
+  },
+};
+
+export { links };
