2a3000cc932db124ddc966243a2b6e4f8d450a77
pavlos noulis <pavlos.noulis@gmail.com>
2023-12-20 18:34:46 +0200

    Turn most envars into required

--- src/config.js
+++ src/config.js
@@ -12,11 +12,16 @@ const __getEnvar = getEnvar.bind(
   ENV,
 );
 
-__getEnvar("MODE");
+__getEnvar("MODE", { required: true });
 __getEnvar("NODE_ENV");
 __getEnvar("LOGLEVEL");
-__getEnvar("DEVICE_ID");
-__getEnvar("AFADMIN_SERVER_URL_ORIGIN_WS", { rename: "AFADMIN_SERVER_URL" });
-__getEnvar("AFADMIN_CLIENT_URL_PUBLIC_BASENAME", { rename: "BASENAME" });
+__getEnvar("DEVICE_ID", { required: true });
+__getEnvar("AFADMIN_SERVER_URL_ORIGIN_WS", {
+  required: true,
+  rename: "AFADMIN_SERVER_URL",
+});
+__getEnvar("AFADMIN_CLIENT_URL_PUBLIC_BASENAME", {
+  rename: "BASENAME",
+});
 
-export { ENV };
+export { ENV, __getEnvar };
