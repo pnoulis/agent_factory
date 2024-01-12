8d7a8cc09962c417baf360787482dd50c01ffc34
pnoul <pavlos.noulis@gmail.com>
2024-01-12 16:48:05 +0200

    listPkgs Route

--- src/afmachine/routes/listPkgs.js
+++ src/afmachine/routes/listPkgs.js
@@ -1,1 +1,16 @@
-import { Route } from "../Route.js";
+import { compose } from "../compose.js";
+import { parseBackendResponse } from "../middleware/parseBackendResponse.js";
+
+export default (backend) =>
+  compose([
+    async function (ctx, next) {
+      ctx.res.data = await backend.listPackages();
+      validateBackendResponse('listPkgs')
+      parseBackendResponse(
+        ctx,
+        "Unable to list agent factory packages",
+        "Successfully retrieved agent factory packages",
+      );
+      await next();
+    },
+  ]);
