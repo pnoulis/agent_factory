ba94ba67db804c7546608eb3b36a94a8d3ab60ab
pavlos noulis <pavlos.noulis@gmail.com>
2024-01-23 21:41:30 +0200

    Eventful follows same logic as Stateful

--- src/Eventful.js
+++ src/Eventful.js
@@ -1,76 +1,101 @@
 import { isFunction } from "js_utils/misc";
 
-class Eventful {
-  constructor(events = []) {
-    this.events = {
-      error: [],
-    };
-    for (const event of events) {
-      this.events[event] = [];
+const eventful = {
+  addEvent(event) {
+    this.events[event] ??= [];
+    return this.events[event];
+  },
+  on(event, options, listener) {
+    if (isFunction(options)) {
+      listener = options;
+      options = {};
     }
-  }
-}
-Eventful.prototype.addEvent = function (event) {
-  this.events[event] ??= [];
-  return this.events[event];
-};
+    const opts = {
+      persist: true,
+      reverse: false,
+      listener,
+      ...options,
+    };
+    const listeners = this.addEvent(event);
 
-Eventful.prototype.on = function (event, options, listener) {
-  if (isFunction(options)) {
-    listener = options;
-    options = {};
-  }
-  const opts = {
-    persist: true,
-    reverse: false,
-    listener,
-    ...options,
-  };
-  const listeners = this.addEvent(event);
-
-  if (opts.reverse) {
-    this.events[event] = [opts, ...listeners];
-  } else {
-    listeners.push(opts);
-  }
-  return this;
-};
-Eventful.prototype.onReverse = function (event, listener) {
-  return this.on(event, { persist: true, reverse: true }, listener);
-};
-
-Eventful.prototype.once = function (event, options, listener) {
-  return isFunction(options)
-    ? this.on(event, { persist: false, reverse: false }, options)
-    : this.on(event, { persist: false, reverse: false, ...options }, listener);
-};
-
-Eventful.prototype.onceReverse = function (event, listener) {
-  return this.on(event, { persist: false, reverse: true }, listener);
-};
-
-Eventful.prototype.emit = function (event, ...args) {
-  if (event === "error" && this.events.error.length < 1) throw args[0];
-  const nextevents = [];
-  for (let i = 0; i < this.addEvent(event).length; i++) {
-    this.events[event][i].listener(...args);
-    if (this.events[event][i].persist) {
+    if (opts.reverse) {
+      this.events[event] = [opts, ...listeners];
+    } else {
+      listeners.push(opts);
+    }
+    return this;
+  },
+  onReverse(event, listener) {
+    return this.on(event, { persist: true, reverse: true }, listener);
+  },
+  once(event, options, listener) {
+    return isFunction(options)
+      ? this.on(event, { persist: false, reverse: false }, options)
+      : this.on(
+          event,
+          { persist: false, reverse: false, ...options },
+          listener,
+        );
+  },
+  onceReverse(event, listener) {
+    return this.on(event, { persist: false, reverse: true }, listener);
+  },
+  emit(event, ...args) {
+    if (event === "error" && this.events.error.length < 1) throw args[0];
+    const nextevents = [];
+    for (let i = 0; i < this.addEvent(event).length; i++) {
+      this.events[event][i].listener(...args);
+      if (this.events[event][i].persist) {
+        nextevents.push(this.events[event][i]);
+      }
+    }
+    this.events[event] = nextevents;
+    return this;
+  },
+  removeListener(event, listener) {
+    if (!this.events.hasOwnProperty(event)) {
+      this.emitError(new Error(`Missing event: '${event}'`));
+    }
+    const nextevents = [];
+    for (let i = 0; i < this.events[event].legth; i++) {
+      if (listener === this.events[event][i]) continue;
       nextevents.push(this.events[event][i]);
     }
-  }
-  this.events[event] = nextevents;
-  return this;
-};
-Eventful.prototype.flush = function (event, listener) {
-  if (!this.events.hasOwnProperty(event)) {
-    this.emitError(new Error(`Missing event: '${event}'`));
-  }
-  const nextevents = [];
-  for (let i = 0; i < this.events[event].legth; i++) {
-    if (listener === this.events[event][i]) continue;
-    nextevents.push(this.events[event][i]);
-  }
-  this.events[event] = nextevents;
+    this.events[event] = nextevents;
+  },
 };
 
-export { Eventful };
+function isEventful(obj) {
+  const _obj = obj?.prototype ?? obj;
+  if (!_obj) return false;
+  for (const fn of Object.keys(eventful)) {
+    if (!(fn in _obj)) return false;
+  }
+  return true;
+}
+
+function createEventful(Base) {
+  Base ??= Object.prototype.constructor;
+
+  class Eventful extends Base {
+    constructor(events, ...args) {
+      super(...args);
+      this.events = {
+        error: [],
+      };
+      for (const e of events) {
+        this.events[e] = [];
+      }
+    }
+  }
+
+  if (!isEventful(Base)) {
+    for (const fn of Object.keys(eventful)) {
+      Eventful.prototype[fn] = eventful[fn];
+    }
+  }
+
+  return Eventful;
+}
+
+export { createEventful, isEventful };
